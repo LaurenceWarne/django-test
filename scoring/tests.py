f5c013424e36717afc5adac0ddd6ecb26320b210
lWarne <laurencewarne@gmail.com>
2021-12-24 18:15:33 +0000

    Add create_score view and tests

--- scoring/tests.py
+++ scoring/tests.py
@@ -32,3 +32,64 @@ class CreateCandidateTestCase(TestCase):
             "/create-candidate", data, content_type="application/json"
         )
         self.assertEqual(response.status_code, 400)
+
+
+class CreateScoreTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.candidate_ref = "score123"
+
+    def test_can_create_score(self):
+        self.client.post(
+            "/create-candidate",
+            {"ref": self.candidate_ref, "name": "foo"},
+            content_type="application/json"
+        )
+        data = {"candidate_ref": self.candidate_ref, "score": "10"}
+        response = self.client.post(
+            "/create-score", data, content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_can_create_multiple_scores_for_candidate(self):
+        self.client.post(
+            "/create-candidate",
+            {"ref": self.candidate_ref, "name": "foo"},
+            content_type="application/json"
+        )
+        data = {"candidate_ref": self.candidate_ref, "score": "10"}
+        response1 = self.client.post(
+            "/create-score", data, content_type="application/json"
+        )
+        response2 = self.client.post(
+            "/create-score", data, content_type="application/json"
+        )
+        self.assertEqual(response1.status_code, 200)
+        self.assertEqual(response2.status_code, 200)
+
+    def test_error_on_inexistant_ref(self):
+        data = {"candidate_ref": "87654321", "score": "10"}
+        response = self.client.post(
+            "/create-score", data, content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_error_on_bad_method(self):
+        response = self.client.get("/create-score")
+        self.assertEqual(response.status_code, 405)
+
+    def test_error_on_bad_json(self):
+        data = {"not-an-identifier": "12345678", "score": "10"}
+        response = self.client.post(
+            "/create-score", data, content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_error_on_bad_float(self):
+        data = {"candidate_ref": self.candidate_ref, "score": "abc"}
+        response = self.client.post(
+            "/create-score", data, content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 400)
+        
--- scoring/views.py
+++ scoring/views.py
@@ -1,6 +1,7 @@
 import json
+from typing import Callable
 
-from django.http import HttpRequest, JsonResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.core.exceptions import ValidationError
 
 from .models import Candidate, Score
@@ -8,30 +9,70 @@ from .models import Candidate, Score
 
 CREATE_CANDIDATE_BAD_JSON_ERR_MSG = "The request JSON must be of the form: " +\
     "{'ref': <ref>, 'name': <name>}"
+CREATE_SCORE_BAD_JSON_ERR_MSG = "The request JSON must be of the form: " +\
+    "{'candidate-ref': <ref>, 'score': <score>}"
 
+View = Callable[[HttpRequest], HttpResponse]
+
+
+def accept_http_method(accepted: str) -> Callable[[str], Callable[[View], View]]:
+    """Return a function wrapping func that returns an error if the
+    HTTP method does not match accepted.
+
+    Keyword arguments:
+    func -- the function to wrap
+    accepted -- the accepted HTTP method
+    """
+    def decorate(func: Callable[[HttpRequest], HttpResponse]):
+        def wrapped(request: HttpRequest) -> HttpResponse:
+            if request.method == accepted:
+                return func(request)
+            else:
+                name = func.__name__
+                error_msg = f"Expected method '{accepted}' for {name}" +\
+                    f"but got '{request.method}'"
+                return JsonResponse({"errors": [error_msg]}, status=405)
+        return wrapped
+    return decorate
+
+
+@accept_http_method("POST")
 def create_candidate(request: HttpRequest) -> JsonResponse:
-    if request.method == "POST":
-        try:
-            data = json.loads(request.body)
-            candidate = Candidate(ref=data["ref"], name=data["name"])
-            candidate.full_clean()
-        except (KeyError, json.JSONDecodeError):
-            return JsonResponse(
-                {"errors": [CREATE_CANDIDATE_BAD_JSON_ERR_MSG]}, status=400
-            )
-        except ValidationError as e:
-            return JsonResponse({"errors": [str(e)]}, status=400)
-        else:
-            candidate.save()
-            return JsonResponse(data)
+    try:
+        data = json.loads(request.body)
+        candidate = Candidate(ref=data["ref"], name=data["name"])
+        candidate.full_clean()
+    except (KeyError, json.JSONDecodeError):
+        return JsonResponse(
+            {"errors": [CREATE_CANDIDATE_BAD_JSON_ERR_MSG]}, status=400
+        )
+    except ValidationError as e:
+        return JsonResponse({"errors": [str(e)]}, status=400)
     else:
-        error_msg = bad_method("create_candidate", "POST", request.method)
-        return JsonResponse({"errors": [error_msg]}, status=405)
+        candidate.save()
+        return JsonResponse(data)
 
 
+@accept_http_method("POST")
 def create_score(request: HttpRequest) -> JsonResponse:
-    return JsonResponse({"Hi!"})
-
-
-def bad_method(resource: str, expected: str, actual: str) -> str:
-    return f"Expected method '{expected}' for {resource} but got '{actual}'"
+    try:
+        data = json.loads(request.body)
+        ref, score_val = data["candidate_ref"], data["score"]
+        candidate = Candidate.objects.filter(ref=ref).first()
+        if candidate is None:
+            error_msg = f"No candidate found with ref '{ref}'"
+            return JsonResponse({"errors": [error_msg]}, status=400)
+        else:
+            score = Score(candidate=candidate, score=float(score_val))
+    except (KeyError, json.JSONDecodeError):
+        return JsonResponse(
+            {"errors": [CREATE_SCORE_BAD_JSON_ERR_MSG]}, status=400
+        )
+    except ValueError:
+        error_msg = f"Expected float for score but found: '{score_val}'"
+        return JsonResponse({"errors": [error_msg]}, status=400)
+    except ValidationError as e:
+        return JsonResponse({"errors": [str(e)]}, status=400)
+    else:
+        score.save()
+        return JsonResponse(data)
